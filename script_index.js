05693944f291af690638cb53b41d4575618be99e
Natalia Chernysheva <chernysn@gmail.com>
2023-08-24 12:44:36 -0400

    added all files from the portfolio folder (main page and projects) 8.24 at 12.44pm

---
+++ script_index.js
@@ -0,0 +1,183 @@
+
+window.addEventListener('load', (evnt) => {
+    document.getElementById("screen").style.cssText = "opacity: 1; transition: 2s; ";
+    document.getElementById("scr_span_one").style.cssText = "opacity: 1; left: 15%; top: 15%; transition: 3s; font-size: 3em; ";
+    document.getElementById("scr_span_two").style.cssText = "opacity: 1; left: 55%; top: 20%; transition: 5s; font-size: 1.5em; ";
+    document.getElementById("scr_span_three").style.cssText = "opacity: 1; left: 25%; top: 35%; transition: 4s; font-size: 2em; ";
+    document.getElementById("enter_btn").style.cssText = "opacity: 1; transition: 2s; transition-delay: 5s"
+})
+
+document.getElementById("enter_btn").addEventListener('click', () => {
+    document.getElementById("screen").style.display = "none";
+
+})
+
+
+
+
+window.onscroll = function () {
+
+    const bar = document.getElementById("prog_bar");
+    let h = document.documentElement;
+    let st = h.scrollTop || document.body.scrollTop;
+    let sh = h.scrollHeight || document.body.scrollHeight;
+    let percent = st / (sh - h.clientHeight) * 100 + 1;
+    let roundedPercept = Math.floor(percent);
+    bar.style.width = roundedPercept + "%";
+}
+
+
+function pop() {
+    document.getElementById("page_overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("page_overlay").style.display = "none";
+}
+
+
+
+const notes_animation = document.querySelectorAll(".note");
+
+let dot_size = [
+    { transform: 'scale(1)', opacity: '0', transition: 'ease-in-out', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(80)', opacity: '.9', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(1)', opacity: '0', top: '0', left: '0', transition: 'ease-in-out', },
+];
+
+let dot_size_rev = [
+    { transform: 'scale(1)', opacity: '0', transition: 'ease-in-out', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(80)', opacity: '.9', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(1)', opacity: '0', bottom: '0', right: '0', transition: 'ease-in-out', },
+];
+
+let dot_size_alt = [
+    { transform: 'scale(1)', opacity: '0', transition: 'ease-in-out', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(80)', opacity: '.9', },
+    { transform: 'scale(40)', opacity: '.6', },
+    { transform: 'scale(1)', opacity: '0', top: '0', right: '0', transition: 'ease-in-out', },
+];
+
+let note_text_color = [
+    { color: 'rgb(45, 45, 45)' },
+    { color: 'rgb(235, 235, 235)', transition: 'ease-in-out', duration: '1s' },
+    { color: 'rgb(45, 45, 45)' },
+];
+
+let img_color_hover = [
+    { opacity: '0' },
+    { opacity: '1' },
+    { opacity: '0' },
+];
+
+let img_color_no_hover = [
+    { opacity: '1' },
+    { opacity: '0' },
+    { opacity: '1' },
+];
+
+let dot_timing = {
+    duration: 3000,
+    iteration: 1,
+};
+
+let note_text_timing = {
+    duration: 3300,
+    iteration: 1,
+};
+
+
+for (i = 0; i < notes_animation.length; i++) {
+    notes_animation[i].addEventListener('mouseenter', function (event) {
+
+        let k = Math.random() * 20;
+
+        if (k > 15) {
+            event.target.querySelector('.backgr_color').animate(dot_size, dot_timing);
+        }
+        else if (k > 8) {
+            event.target.querySelector('.backgr_color').animate(dot_size_rev, dot_timing);
+        } else {
+            event.target.querySelector('.backgr_color').animate(dot_size_alt, dot_timing);
+        };
+
+        event.target.querySelector(".note_text").animate(note_text_color, note_text_timing);
+        event.target.querySelector(".note_comment").animate(note_text_color, dot_timing);
+        event.target.querySelector(".note_bottom").animate(note_text_color, dot_timing);
+        event.target.querySelector(".note_img").animate(img_color_no_hover, dot_timing);
+        event.target.querySelector(".note_img_hover").animate(img_color_hover, dot_timing);
+
+
+    });
+}
+
+
+let form = document.querySelector('form');
+form.addEventListener('submit', submit_form);
+
+function submit_form(e) { e.preventDefault(); };
+
+
+/*
+
+
+for (i = 0; i < notes_animation.length; i++) {
+    notes_animation[i].addEventListener(my_fun(), function (event) {
+        console.log("Class removed");
+        event.target.querySelector('.backgr_color').classList.remove("run_notes_animation");
+    });
+}
+
+
+    x.target.querySelector(".note_text").classList.remove("run_text_animation");
+    x.target.querySelector(".note_comment").classList.remove("run_text_animation");
+    x.target.querySelector(".note_bottom").classList.remove("run_text_animation");
+ 
+*/
+
+let chat_btn = document.getElementById('pulse');
+let chat = document.getElementById('chat');
+
+chat_btn.addEventListener('click', () => {
+    if (chat.style.display == 'flex') {
+
+        chat.style.display = 'none'
+    } else {
+        chat.style.display = 'flex'
+    }
+})
+
+let chat_send_btn = document.getElementById("chat_send_btn");
+let new_msg = document.getElementById("chat_texting_input");
+let chat_body = document.getElementById("chat_body");
+let bot_message = "Hello! How can I assist you today?"
+
+
+
+chat_send_btn.addEventListener('click', () => {
+    if (new_msg.value != '') {
+        let new_text_msg = document.createElement('div');
+        new_text_msg.classList.add("chat-bubble-me");
+        let text_in_msg = document.createTextNode(new_msg.value);
+        new_text_msg.appendChild(text_in_msg);
+        chat_body.appendChild(new_text_msg);
+        new_msg.value = '';
+        
+        setTimeout(() => {new_text_msg = document.createElement('div');
+        new_text_msg.classList.add("chat-bubble-other");
+        text_in_msg = document.createTextNode(bot_message);
+        new_text_msg.appendChild(text_in_msg);
+        chat_body.appendChild(new_text_msg);}, 3000);
+
+    }
+})
+
+
+
+
+
---
+++ script_projects.js
@@ -0,0 +1,232 @@
+
+window.addEventListener('resize', () => {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    console.log(w, "and", h)    
+
+})
+
+
+/* HOME BUTTON AND NAVIGATION ============================================================ */
+
+let next_btn = document.getElementById("btn_next_right");
+let back_btn = document.getElementById("btn_back_left");
+let section_1 = document.getElementById("section_1");
+let section_2 = document.getElementById("section_2");
+let section_3 = document.getElementById("section_3");
+let section_4 = document.getElementById("section_4");
+section_1.style.left = "0%";
+section_2.style.left = "-100%";
+section_3.style.left = "-100%";
+section_4.style.left = "-100%";
+
+
+next_btn.addEventListener('click', () => {
+
+    if (section_1.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "0%"; section_3.style.left = "-100%"; section_4.style.left = "-100%"; }
+    else if (section_2.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "-100%"; section_3.style.left = "0%"; section_4.style.left = "-100%"; }
+    else if (section_3.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "-100%"; section_3.style.left = "-100%"; section_4.style.left = "0%"; }
+    else if (section_4.style.left == "0%") { section_1.style.left = "0%"; section_2.style.left = "-100%"; section_3.style.left = "-100%"; section_4.style.left = "-100%"; }
+    else { console.log("Hello") }
+
+})
+
+back_btn.addEventListener('click', () => {
+
+    if (section_1.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "-100%"; section_3.style.left = "-100%"; section_4.style.left = "0%"; }
+    else if (section_2.style.left == "0%") { section_1.style.left = "0%"; section_2.style.left = "-100%"; section_3.style.left = "-100%"; section_4.style.left = "-100%"; }
+    else if (section_3.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "0%"; section_3.style.left = "-100%"; section_4.style.left = "-100%"; }
+    else if (section_4.style.left == "0%") { section_1.style.left = "-100%"; section_2.style.left = "-100%"; section_3.style.left = "0%"; section_4.style.left = "-100%"; }
+    else { console.log("Hello") }
+
+})
+
+
+let home_btn = document.getElementById("home_btn");
+home_btn.addEventListener('mouseover', () => {
+
+})
+
+
+
+/* CALCULATOR ============================================================
+*/
+
+
+optionMenu = document.querySelector(".compounded_select"),
+    selectBtn = optionMenu.querySelector(".compounded_btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn_text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option_text").innerText;
+        sBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove("active");
+
+    })
+})
+
+
+function total() {
+    let p = document.getElementById("current_investment").value;
+    let d = document.getElementById("monthly_deposits").value;
+    let R = document.getElementById("rate_of_return").value;
+    let n = document.querySelector(".sBtn_text").innerText;
+
+    if (n == "annually") {
+        n = 1;
+    }
+
+    else if (n == "quaterly") {
+        n = 4;
+    }
+
+    else if (n == "monthly") {
+        n = 12;
+    }
+
+    else {
+        n = 0;
+    }
+
+    let t = document.getElementById("period_months").value;
+
+    p = parseFloat(p);
+    R = parseFloat(R);
+    n = parseFloat(n);
+    t = parseFloat(t);
+
+    let r = R / 100;
+
+    let A = p * (1 + r / n) ** n * t;
+    A = Math.floor(A);
+    let XY = Intl.NumberFormat('en-EN', { style: 'currency', currency: 'USD' }).format(A);
+
+    document.getElementById("total_final").innerHTML = XY;
+
+}
+
+
+
+/* SECOND SECTION ================================================================*/
+
+let date = new Date();
+date = date.toString().split(" ");
+let today = `${date[2]} ${date[1]} ${date[3]}`;
+
+
+
+let input_array = JSON.parse(localStorage.getItem("items")) || [];
+display_items(input_array);
+
+console.log(localStorage);
+
+/* LOCAL STORAGE CLEAR, CLOSE */
+
+document.getElementById("clear").addEventListener("click", () => {
+    localStorage.clear();
+    console.log(localStorage)
+    input_array = []
+    $('.section_2').load('projects.html #section_2');
+    display_items(input_array);
+
+})
+
+
+/* LOCAL STORAGE AND TODO INPUT */
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+document.getElementById("todo_btn_add").addEventListener('click', () => {
+    let input = document.getElementById("todo_input").value;
+
+    if (input == "") {
+        let alert = document.getElementById("alert");
+        alert.style.display = 'block';
+    }
+    else {
+        let id = Math.floor(Math.random() * 100000)
+
+        let task = {
+            id: id,
+            name: input,
+        }
+
+        create_local_item(task);
+        document.getElementById("todo_input").value = "";
+    }
+})
+
+/* DATE */
+
+
+
+
+function create_local_item(task) {
+    input_array.push(task);
+    localStorage.setItem("items", JSON.stringify(input_array));
+    console.log(localStorage);
+    display_items(input_array);
+}
+
+function display_items(task) {
+    let items = "";
+    for (y = 0; y < input_array.length; y++) {
+        items += `                        
+            <div class="my_todo_items"><span class="today" id="today">${today}</span><span class="items">${task[y].name}</span><button class="remove_item" onclick="delete_btn()"> x </button></div>`
+    }
+    document.getElementById("my_list").innerHTML = items;
+}
+
+
+
+
+
+function delete_btn() {
+    let all_buttons = document.querySelectorAll(".remove_item");
+    all_buttons.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            truly_delete(i)
+        })
+    })
+}
+
+function truly_delete(i) {
+    input_array.splice(i, 1);
+    localStorage.setItem("items", JSON.stringify(input_array));
+    console.log(localStorage);
+    input_array = JSON.parse(localStorage.getItem("items"));
+    $('.section_2').load('projects.html #section_2');
+    console.log(input_array);
+    display_items(input_array);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
